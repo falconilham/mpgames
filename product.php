6eb82dc513e9319606363c1649d9ea2f0e4f946a
falconilham <ilhama36@gmail.com>
2018-12-11 01:03:54 +0700

    produk

---
+++ product.php
@@ -0,0 +1,64 @@
+<html>
+    <header>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="asset/css/mystyle.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="asset/js/bootstrap.min.js"></script>
+        <script src="asset/jquery/jquery-3.2.1.min.js"></script>
+        <script src="asset/bootstrap/js/bootstrap.min.js"></script>
+
+        <div class="topnav">
+            <a onclick="reload()" href=".">Home</a>
+            <a href="#about">Jual/Tukar</a>
+            <a href="login.php" style="float: right;">Login</a>
+            <div class="search-container">
+                <form action="/action_page.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div>
+    </header>
+    <body>
+        <div id="container">
+            <div class="list_transaksi">
+            <h4>Assassins Creed Odyssey</h4>
+                <div id="product">
+                    <div id="gambar">
+                        <img src="img/ac.jpg">
+                    </div>
+                    
+                    <div id="keterangan">
+                        
+                        Harga<h3>RP 300.000</h3>
+
+                        Penjual<h3>ilham</h3>
+                        Lokasi<h3>Jakarta</h3>
+                        Nomor Telepon <h3>08121212</h3>
+                        <input type="checkbox" checked="checked" name="WA" disabled/>Whatsapp Available<br>
+                        <input type="checkbox" checked="checked" name="call" disabled/> Call Available
+                    </div>
+                    <div class="deskripsi"><h4 style="text-align: center;">Trailer & Description</h4>
+                    <div class="garis"></div>
+                        <iframe width="350" height="259" src="https://www.youtube.com/embed/s_SJZSAtLBA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    asdasdas
+                    </div>
+                 </div>
+
+                <div class="rekomendasi">
+                <div class="garis-judul"></div>
+                <h2 style="text-align: center;"> Rekomendasi Game Untuk Anda </h2>
+                <div class="game-wts" style="margin-top: 2px">
+                    <a href="#">
+                        <img src="img/ac.jpg">
+                        </a>
+                        <div class="judul-game">Assassins Creed
+                            <p>RP 500.000
+                                jakarta
+                    </a></div>
+                </div>
+            </div>
+        </div>        
+    </body>
+</html>
