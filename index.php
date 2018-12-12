9b9c7fc91aa185542b2e9908af29c3b73b0ecc0a
falconilham <ilhama36@gmail.com>
2018-12-12 22:04:27 +0700

    masih ada yang salah di session

--- index.php
+++ index.php
@@ -11,7 +11,14 @@
         <div class="topnav">
             <a class="active" onclick="reload()" href="">Home</a>
             <a href="#about">Jual/Tukar</a>
-            <a href="login.php" style="float: right;">Login</a>
+            <?php
+                if (empty($_SESSION['id_user'])){
+                    echo '<a href="login.php" style="float: right;">login</a>';
+                }
+
+                else {
+                    echo '<a href="proses/logout.php" style="float: right;">Logout</a>';
+                } ?>
             <div class="search-container">
                 <form action="/action_page.php">
                     <input type="text" placeholder="Search.." name="search">
