5c8fbedbbb3c240648a557319cc4ac541b697d4c
falconilham <ilhama36@gmail.com>
2018-12-14 02:21:22 +0700

    footer

--- index.php
+++ index.php
@@ -65,14 +65,26 @@ session_start();
                 </div>
                 <div class="pagination">
                     <a href="#">&laquo;</a>
-                    <a href="#">1</a>
-                    <a href="#" class="active">2</a>
+                    <a href="#" class="active">1</a>
+                    <a href="#">2</a>
                     <a href="#">3</a>
                     <a href="#">4</a>
                     <a href="#">5</a>
                     <a href="#">&raquo;</a>
                 </div>
             </div>
+        
+        <div class="footer">
+            <div class="footer-column">
+                layanan Pelanggan
+                <ul>
+                    <li>Bantuan</li>
+                    <li>Term And Condition</li>
+                </ul>
+            </div>
+        </div>    
+            
+
             </div>
             <script src="asset/js/main.js"></script>
         </div>
